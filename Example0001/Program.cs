67411228beba1896d064beba24baa50c2588f312
Kate Raf <KateSu1989@gmail.com>
2022-09-19 23:19:43 +0500

    Debug

---
+++ Example0001/Program.cs
@@ -0,0 +1,30 @@
+﻿///void Insert()
+//{
+//Console.WriteLine("Вставить");
+//}
+
+int CreateNumber()
+{
+   int number = new Random().Next(10,100); 
+   return number;
+}
+
+int MaxDigit(int number);
+{
+    int Decin = number / 10;
+    int Edinic = number % 10;
+    
+    if (Decin >= Edinic)
+    {
+        return Decin;
+    }
+    else 
+    {
+        return Edinic;
+    }
+  }
+
+int a = CreateNumber();
+Console.WriteLine(a);
+int maxDigit = MaxDigit(a);
+Console.WriteLine(maxDigit);
