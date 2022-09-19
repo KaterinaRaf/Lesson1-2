92ed8ffd74061679fea59691cb9d05430aeb8e47
Kate Raf <KateSu1989@gmail.com>
2022-09-19 23:12:45 +0500

    Work1

---
+++ Work1/Program.cs
@@ -0,0 +1,15 @@
+﻿// работа в группе 1
+int CreateNumber()
+{
+    return new Random().Next(100,1000);
+}
+int DeleteSecondDigit(int Number)
+{
+    int Hundred = Number / 100;
+    int Ed = Number % 10;
+    return (Hundred*10+Ed);
+}
+
+int Number = CreateNumber();
+Console.WriteLine(Number);
+Console.WriteLine(DeleteSecondDigit(Number));
