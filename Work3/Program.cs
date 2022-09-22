30affa8bbb085d994fce220ecc46eec742cdcac5
Kate Raf <KateSu1989@gmail.com>
2022-09-22 23:22:45 +0500

    Work 3

---
+++ Work3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+//и находит расстояние между ними в 2D пространстве.
+// Math.Sqrt(int a); Math.Pow(x,2);
+
+double GetNumber(string name) // метод для ввода чисел
+{
+    Console.WriteLine("Введите координату " + name);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double x1 = GetNumber("x1");
+double x2 = GetNumber("x2");
+double y1 = GetNumber("y1");
+double y2 = GetNumber("y2");
+
+double S = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2));
+Console.WriteLine("Расстояние = " + S);
