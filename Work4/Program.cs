9573963d1561366d6139316df7c5e91ef320bffe
Kate Raf <KateSu1989@gmail.com>
2022-09-22 23:46:59 +0500

    Work 4

---
+++ Work4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.WriteLine("Введите число!");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void Tab(int a)
+{
+    int i = 0;
+    while (i < a)
+    {
+        Console.WriteLine(Math.Pow(i,2));
+        i++;
+    }
+}
+Tab(N);
