3778f187be75091053aa16465dc8518ae022b7d1
Kate Raf <KateSu1989@gmail.com>
2022-09-22 22:57:41 +0500

    Lesson 3

---
+++ Example0002/Program.cs
@@ -0,0 +1,54 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, 
+//кратно ли оно одновременно 7 и 23.
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число!");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool CheckMod(int a, int mod)
+{
+    return (a % mod ==0);
+}
+
+int a = GetNumber();
+if (CheckMod(a,7) && CheckMod(a,23))
+Console.WriteLine("Верно!");
+else
+Console.WriteLine("Не верно!");
+
+// bool result;
+// bool del7;
+// bool del23;
+
+// result = (del7==true && del23==true);
+
+// Console.WriteLine("Верно!");
+
+//if (a % 7 ==0)
+//{
+//del7 = true;
+//}
+//else
+//{
+//    del7 = false;
+//}
+//if (a % 23 ==0)
+//{
+//del23 = true;
+//}
+//else
+//{
+//    del23 = false;
+//}
+//if (del7==true && del23==true)
+//{
+//    result = true;
+//}
+//else
+// {
+//     result = false;
+// }
+// if (result ==true)
+//Console.WriteLine("Верно!");
---
+++ Example0003/Program.cs
@@ -0,0 +1,28 @@
+﻿// 17. Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, 
+//в которой находится эта точка.
+
+double GetNumber(string name) // метод для ввода чисел
+{
+    Console.WriteLine("Введите координату " + name);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+int GetNumChet(double x, double y) // метод для расчета
+{
+    if (x > 0 && y > 0) // логическое И
+    return 1; // первая четверть
+    else
+    if (x < 0 && y > 0) 
+    return 2; 
+    else
+    if (x < 0 && y < 0) 
+    return 3; 
+    else
+    return 4; 
+}
+
+double x = GetNumber("x");
+double y = GetNumber("y");
+
+Console.WriteLine("Четверть: " + GetNumChet(x,y));
