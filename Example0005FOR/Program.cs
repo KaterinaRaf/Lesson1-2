4ddc7fceff6ae803bf736218b030fcb0d0b0c789
Kate Raf <KateSu1989@gmail.com>
2022-09-26 22:28:47 +0500

    Examlpe 0005 For

---
+++ Example0005FOR/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число (А) и 
+//выдаёт сумму чисел от 1 до А. 7 -> 28; 4 -> 10; 8 -> 36
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число А");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSum1toA(int A)
+{
+    int sum = 0;
+    for (int i = 1; i <= A; i++)
+    {
+      sum += i;
+    }
+    return sum;
+}
+
+int A = GetNumber();
+Console.WriteLine("Сумма чисел от 1 до " + A + " равна " + GetSum1toA(A));
