1c11e3de76b208a7d8a09b2b9d3fce7e202d458c
Kate Raf <KateSu1989@gmail.com>
2022-09-26 22:13:05 +0500

    Lesson 4

---
+++ Example0004/Program.cs
@@ -0,0 +1,12 @@
+﻿int GetNumberThreeDigit() // метод для ввода чисел
+{
+    int num = 0;
+    while (num < 99 || num > 999)
+    {
+        Console.WriteLine("Введите трехзначное число!");
+        num = Convert.ToInt32(Console.ReadLine());
+    }
+    return num;
+}
+int a = GetNumberThreeDigit();
+Console.WriteLine(a);
