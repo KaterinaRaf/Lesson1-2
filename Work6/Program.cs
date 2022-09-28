6ab866986fedeccd74d3f81ab2f72ae6bd7ec9b3
Kate Raf <KateSu1989@gmail.com>
2022-09-28 22:43:23 +0500

    Work 6

---
+++ Work6/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 28: Напишите программу, которая принимает на вход число N 
+//и выдаёт произведение чисел от 1 до N. 4 -> 24    5 -> 120
+
+int GetNumber()
+{
+            Console.WriteLine("Введите число: ");
+      return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetProduct (int number)
+{
+    int product = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        product = product * i;
+    }
+    return product;
+}
+
+int number = GetNumber();
+Console.WriteLine("Произведение цифр равно: " + GetProduct(number));
