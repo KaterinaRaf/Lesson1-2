7ce03c0fa61b2745d4cf165766b9849c60e0674f
Kate Raf <KateSu1989@gmail.com>
2022-10-04 22:59:09 +0500

    Защита от пользователя

---
+++ Example008Taboo/Program.cs
@@ -0,0 +1,38 @@
+﻿// метод для поиска 
+
+int GetNumber()
+//{
+    //*******первый способ******
+    //string text = ""; //текст, который ввел пользователь
+    //do 
+    // {
+        
+    //     text = Console.ReadLine(); // проверка 
+    // }
+    // while (text.IndexOf(',') >= 0 || text.IndexOf('.') >= 0);
+//     return Convert.ToInt32(text); // если нет лишних знаков (точки или зпт)
+// }
+//Console.WriteLine(GetNumber());
+    
+    //*****второй способ с текстовым оповещением******
+{
+    string text = ""; //текст, который ввел пользователь
+    bool key = true;
+    while (key)
+    {
+        text = Console.ReadLine();
+        if (text.IndexOf(',') >= 0 || text.IndexOf('.') >= 0) // проверка на наличие символов введеных пользователем
+        {
+            Console.WriteLine("Введите верное число!!!");
+            key = true;
+        }
+        else
+        {
+            key = false;
+        }
+    }
+
+    return Convert.ToInt32(text); // если нет лишних знаков (точки или зпт)
+}
+
+Console.WriteLine(GetNumber());
