e99043efd34f1fc55e3814328417c5f0358307fc
Kate Raf <KateSu1989@gmail.com>
2022-10-08 15:14:00 +0500

    Interpolyaciya and Double Random

---
+++ Exmaple009/Program.cs
@@ -0,0 +1,15 @@
+﻿// Интерполяция вывода строк через $ и {имя_переменной}
+
+int a = 5;
+int b = 2;
+
+Console.WriteLine($"Переменные: а={a}, b={b}");
+
+//рандом вещественных (с дробной частью) чисел от 0 до 100
+
+double number = new Random().NextDouble();  //в скобках нельзя выбирать число
+double number1 = new Random().NextDouble(); // разряды выбираются путем умножения
+double number2 = new Random().NextDouble(); // на 10, 100, 1000, ....
+
+Console.WriteLine($"Числа: {Math.Round(number*100, 2)}, {number1*100}, {number2*100}");
+
