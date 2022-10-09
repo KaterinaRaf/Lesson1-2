09cd8827bace480f4be93b6bc5ff3c8f0f6fa2e8
Kate Raf <KateSu1989@gmail.com>
2022-10-09 20:31:42 +0500

    Exercise 046

---
+++ Example011/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+int GetNumberFromConsole(string text) //пользовательский ввод данных
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetRandomValue(int minValue, int maxValue)
+{
+    return new Random().Next(minValue, maxValue + 1);
+}
+
+void FillArray(int[,] arr, int minValue, int maxValue) // заполнение двумерного массива
+{
+    for(int i = 0; i < arr.GetLength(0); i++) // длина массива в строках
+    {
+        for(int j = 0; j < arr.GetLength(1); j++) // длина массива в столбце
+        {
+            arr[i,j] = GetRandomValue(minValue,maxValue); // принимает значения из метода
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] InitializateArray(int m, int n) // создание массива. int[,] = возвращает двумерный массив
+{
+    return new int[m,n];
+}
+
+//КОД ОСНОВНОЙ ПРОГРАММЫ
+int m = GetNumberFromConsole("Введите размерность массива m");
+int n = GetNumberFromConsole("Введите размерность массива n");
+int[,] array = InitializateArray(m,n);
+int minValue = GetNumberFromConsole("Введите минимальное число, допустимое в массиве");
+int maxValue = GetNumberFromConsole("Введите максимальное число, допустимое в массиве");
+FillArray(array,minValue,maxValue);
+
+PrintArray(array);
+
+
+
