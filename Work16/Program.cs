1b4cad5cb54ff0994a77e226b92db7c19fd0953f
Kate Raf <KateSu1989@gmail.com>
2022-10-11 14:34:43 +0500

    Why don't work?

---
+++ Work16/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задать массив случайных чисел m*n
+// Затем, транспортировать массив, превратив в новый, размером n*m
+
+int GetNumberFromConsole(string text) //пользовательский ввод данных
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++) // длина массива в строках
+    {
+        for(int j = 0; j < arr.GetLength(1); j++) // длина массива в столбце
+        {
+            arr[i,j] = i + j; // принимает значения из метода
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] InitializateArray(int m, int n) // создание массива. int[,] = возвращает двумерный массив
+{
+    return new int[m,n];
+}
+
+int[,] TranspositionArray(int[,] arr)
+{
+    int[,] result = new int[arr.GetLength(1), arr.GetLength(0)];
+    for(int i = 0; i < arr.GetLength(0); i++)
+        for(int j = 0; j < arr.GetLength(1); j++)
+            result[j, i] = arr[i, j];
+    return result;
+}
+
+//КОД ОСНОВНОЙ ПРОГРАММЫ
+int m = GetNumberFromConsole("Введите размерность массива m"),
+    n = GetNumberFromConsole("Введите размерность массива n");
+int[,] array = InitializateArray(m,n);
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int[,] array2 = TranspositionArray(array);
+Console.WriteLine("Новый массив: " +array2);
