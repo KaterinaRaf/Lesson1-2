722bccc8a4f2f2241873a87a7e5b5867194eaafe
Kate Raf <KateSu1989@gmail.com>
2022-10-11 22:25:56 +0500

    Recursia

--- Example012/Program.cs
+++ Example012/Program.cs
@@ -1,2 +1,36 @@
-﻿// 
+﻿// Задача про рекурсию.
+// Написать рекурсивный метод деления одного числа на другое.
+// Нужно получить остаток от деления и результат деления
 
+int InputNumber(string text) //пользовательский ввод данных
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int DivisionRemainder(int number, int divider)
+{
+    if (number >= divider)
+    {
+        number -= divider;
+        return DivisionRemainder(number, divider);
+    }
+    else 
+        return number;
+}
+
+int DivisionFull(int number, int divider)
+{
+    if (number >= divider)
+    {
+        number -= divider;
+        return DivisionFull(number, divider) + 1;
+    }
+    else 
+        return 0;
+}
+
+int number = InputNumber("Введите числитель: "),
+    divider = InputNumber("Введите знаменатель: ");
+
+Console.WriteLine($"{number}/{divider}={DivisionFull(number, divider)}, остаток {DivisionRemainder(number, divider)}");
