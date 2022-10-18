69d1a62b41bbcc74bbf3a6dc32c8cee705a8b19c
KaterinaRaf <113040268+KaterinaRaf@users.noreply.github.com>
2022-10-18 22:50:23 +0500

    Interpolyaciya and Double Random

--- Exmaple009/Program.cs
+++ Exmaple009/Program.cs
@@ -1,4 +1,4 @@
-﻿// Интерполяция вывода строк через $ и {имя_переменной}
+﻿// Интерполяция вывода строк через $ и {имя_переменной} так правильнее оформлять
 
 int a = 5;
 int b = 2;
@@ -12,4 +12,3 @@ double number1 = new Random().NextDouble(); // разряды выбираютс
 double number2 = new Random().NextDouble(); // на 10, 100, 1000, ....
 
 Console.WriteLine($"Числа: {Math.Round(number*100, 2)}, {number1*100}, {number2*100}");
-
